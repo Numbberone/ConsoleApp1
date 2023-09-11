25936a635ed751cc50d3827751f705fdd1a715b5
Dari <bodeeva2001@gmail.com>
2023-09-11 10:17:02 +0800

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+ int[] Numbers = new int[] { 1, 10, 20, 30 };
+int sum  = Numbers.Sum();
+Console.WriteLine("Сумма чисел" + sum);
